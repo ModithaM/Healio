a0986289078fd918c8e17163fcf18bcf52a93e0e
nipun-munasinghe <nipunlakmal884@gmail.com>
2026-04-14 10:05:54 +0530

    feat: create doctor profile response

---
+++ services/doctor-service/src/main/java/com/healio/doctorservice/dto/DoctorProfileResponseDto.java
@@ -0,0 +1,29 @@
+package com.healio.doctorservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DoctorProfileResponseDto {
+
+    private String id;
+    private String userId;
+    private String specialization;
+    private String licenseNumber;
+    private String qualifications;
+    private Integer experienceYears;
+    private BigDecimal consultationFee;
+    private String verificationStatus;
+    private List<DoctorAvailabilityResponseDto> availabilitySlots;
+    private UserDto userInfo;
+}
